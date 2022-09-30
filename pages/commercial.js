fa13da8f725a935dc54504b98d75e64c0bdff0fe
Thetoyking91 <59570607+Thetoyking91@users.noreply.github.com>
2022-09-30 08:26:29 -0700

    Update commercial.js

--- pages/commercial.js
+++ pages/commercial.js
@@ -1,1 +1,12 @@
-
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { Navbar } from '../public/navbar'
+import Template from '../public/Template'
+export default function Home() {
+  return (
+    <div>
+      <Template title="Template" headText="Welcome to Template" desc="Template" />
+    </div>
+  )
+}
