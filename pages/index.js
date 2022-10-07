a75368a32a41bb2710e38cee46fda843c936b458
Thetoyking91 <59570607+Thetoyking91@users.noreply.github.com>
2022-10-07 08:15:35 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,12 @@ export default function Home() {
   return (
     <div>
       <Template title="Home" headText="This website will be discussing quagga mussels." desc="Quagga Mussels" >
-        <p> Quagga mussels are a very dangerous invasive species. </p>
+        <p> Quagga mussels are a very dangerous invasive species for the following reasons: </p>
+    <ul>
+    <li>
         <p> Quagga mussels can produce 30,000-40,000 eggs in one breeding cycle, or 1 million per year! </p>
+    </li>
+    </ul>
       </Template>
     </div>
   )
