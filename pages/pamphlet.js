d314d86ac12f321c992aca90682e016a5ccf9dc4
Thetoyking91 <59570607+Thetoyking91@users.noreply.github.com>
2022-10-21 08:11:14 -0700

    Update pamphlet.js

--- pages/pamphlet.js
+++ pages/pamphlet.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div>
       <Template title="Home" headText="This website will be discussing quagga mussels." desc="Quagga Mussels" >
         <p> This is a pamphlet about quagga mussels. </p>
-    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQox_SrWeggIptencABeiHPwIjR3UYQmEo3m5EQ2qZR9Dw3kGnA0aiG_cwNJh8LthJwBDP7R_RavmeR/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQox_SrWeggIptencABeiHPwIjR3UYQmEo3m5EQ2qZR9Dw3kGnA0aiG_cwNJh8LthJwBDP7R_RavmeR/embed?start=false&loop=false&delayms=3000" frameBorder="0" width="960" height="569" allowFullScreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
       </Template>
     </div>
   )
