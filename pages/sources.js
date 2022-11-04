7da20bee5a932b88ff7ba8cea14ad5b9ff7deb59
Thetoyking91 <59570607+Thetoyking91@users.noreply.github.com>
2022-11-04 08:32:30 -0700

    Create sources.js

---
+++ pages/sources.js
@@ -0,0 +1,27 @@
+
+
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { Navbar } from '../public/navbar'
+import Template from '../public/Template'
+import Link from 'next/link'
+export default function Home() {
+  return (
+    <div>
+      <Template title="Credits | Quagga Mussel Awareness" headText="Sources:" desc="Quagga Mussels" >
+    <ul className="text-left">
+    <li>
+        <Link href="https://extension.arizona.edu/sites/extension.arizona.edu/files/pubs/az1481e.pdf"> <p> U of A </p> </Link>
+    </li>
+    <li>
+      Quagga mussels can attach to soft things, while zebra mussels can only attach to hard surfaces.
+    </li>
+    <li>
+      Quagga mussels are freshwater mussels, so they can only survive in freshwater and not the ocean.
+    </li>
+    </ul>
+      </Template>
+    </div>
+  )
+}
