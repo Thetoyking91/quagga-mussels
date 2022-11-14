719001ad18e40dd0d9ee5a830501ecf90fa78b3b
Thetoyking91 <59570607+Thetoyking91@users.noreply.github.com>
2022-11-14 08:26:55 -0700

    Update navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -8,6 +8,7 @@ export class Navbar extends React.Component {
             <Link href="/commercial"><a className="text-xl text-blue-600">Commercial</a></Link>
                 <Link href="/pamphlet"><a className="text-xl text-blue-600">Pamphlet</a></Link>
                     <Link href="/sources"><a className="text-xl text-blue-600">Sources</a></Link>
+                        <Link href="/authors"><a className="text-xl text-blue-600">Authors</a></Link>
         </nav>
     }
 }
