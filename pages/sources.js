58d82a014968e6be30490c93fa63c4eff659ce2e
Thetoyking91 <59570607+Thetoyking91@users.noreply.github.com>
2022-11-14 08:11:08 -0700

    Update sources.js

--- pages/sources.js
+++ pages/sources.js
@@ -15,19 +15,19 @@ export default function Home() {
        <a href="https://extension.arizona.edu/sites/extension.arizona.edu/files/pubs/az1481e.pdf" passHref={true}> <p> U of A </p> </a>
     </li>
     <li>
-      <a href="https://extension.arizona.edu/sites/extension.arizona.edu/files/pubs/az1481e.pdf" passHref={true}> <p> Wisconsin Sea Grant </p> </a>
+      <a href="https://www.seagrant.wisc.edu/our-work/focus-areas/ais/invasive-species/invasive-species-fact-sheets/mollusks/quagga-mussel/" passHref={true}> <p> Wisconsin Sea Grant </p> </a>
     </li>
     <li>
-      <a href="https://extension.arizona.edu/sites/extension.arizona.edu/files/pubs/az1481e.pdf" passHref={true}> <p> National Invasive Species Information Center </p> </a>
+      <a href="https://www.invasivespeciesinfo.gov/aquatic/invertebrates/quagga-mussel" passHref={true}> <p> National Invasive Species Information Center </p> </a>
     </li>
     <li>
-      <a href="https://extension.arizona.edu/sites/extension.arizona.edu/files/pubs/az1481e.pdf" passHref={true}> <p> Aquatic Invasive Species </p> </a>
+      <a href="https://www.azgfd.com/fishing/invasivespecies/quaggamussels/" passHref={true}> <p> Aquatic Invasive Species </p> </a>
     </li>
     <li>
-      <a href="https://extension.arizona.edu/sites/extension.arizona.edu/files/pubs/az1481e.pdf" passHref={true}> <p> University of Montana </p> </a>
+      <a href="https://flbs.umt.edu/newflbs/media/1837/ais-lesson_4_invasive-mussel-life-cycle-poster.pdf" passHref={true}> <p> University of Montana </p> </a>
     </li>
     <li>
-      <a href="https://extension.arizona.edu/sites/extension.arizona.edu/files/pubs/az1481e.pdf" passHref={true}> <p> Wikipedia </p> </a>
+      <a href="https://en.wikipedia.org/wiki/Quagga_mussel" passHref={true}> <p> Wikipedia </p> </a>
     </li>
     </ul>
       </Template>
