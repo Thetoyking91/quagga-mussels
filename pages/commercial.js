cc56ab4324bdfe70d8e33a64c062eaaf3da1cfd5
Thetoyking91 <59570607+Thetoyking91@users.noreply.github.com>
2022-11-15 19:11:17 -0700

    Commercial attempt 2

--- pages/commercial.js
+++ pages/commercial.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Template title="Commercial | Quagga Mussel Awareness" headText="" desc="This is the commercial" >
-        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/cDh2GhNQfA8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/cDh2GhNQfA8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </Template>
     </div>
   )
