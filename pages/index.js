be0d926485459243d0fcf9ca6c2ff7dd64087b4c
Thetoyking91 <59570607+Thetoyking91@users.noreply.github.com>
2022-11-17 17:05:02 -0700

    IMAGES V12

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
             Check your boat for quagga mussels to prevent them from spreading.
           </li>
         </ul>
-        <div>
+        <div className="grid grid-cols-3 gap-4">
           <Image src={image1} className="gallery" />
           <Image src={image2} className="gallery" />
           <Image src={image3} className="gallery" />
